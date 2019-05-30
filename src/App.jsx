44f6a364129d1de9edc7e0649e0ea4d0ed1c5998
Zeeshan <sk.zeeshan1992@gmail.com>
2019-05-29 20:34:09 -0400

    Api Key add also form title & weather file created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { Component } from "react";
+import Title from "./Component/Title.jsx";
+import Form from "./Component/Form.jsx";
+import Weather from "./Component/Weather.jsx";
+const API_KEY = "2d6c038d2a03b7478919c4c5d35651ba";
 
 export default class App extends Component {
+  weather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=Montreal,ca&appid=${API_KEY}&units=metric`
+    );
+  };
   render() {
-    return <div>hello</div>;
+    return (
+      <div>
+        <Title />
+        <Form />
+        <Weather />
+      </div>
+    );
   }
 }
---
+++ src/Component/Title.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Title extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather Finder</h1>
+        <p>Find out temperature, humidity and more ...</p>
+      </div>
+    );
+  }
+}
