0cbb596948ba89ab85a5fa9c10701e416295f3b3
Zeeshan <sk.zeeshan1992@gmail.com>
2019-05-30 22:18:14 -0400

    fixed double image issue

--- src/Component/Title.jsx
+++ src/Component/Title.jsx
@@ -4,7 +4,7 @@ export default class Title extends Component {
   render() {
     return (
       <div>
-        <h1 className="title-container">Weather Finder</h1>
+        <h1>Weather Finder</h1>
         <p>Find out temperature, humidity and more ...</p>
       </div>
     );
